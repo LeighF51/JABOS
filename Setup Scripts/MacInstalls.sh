19a121c139936d11df9bf64f885c764576e6aeb0
jesmaail <josephesmaail93@gmail.com>
2021-06-13 01:18:12 +0100

    Add conditional for dev tools install in MacInstalls.sh

--- Setup Scripts/MacInstalls.sh
+++ Setup Scripts/MacInstalls.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Homebrew Script for MacOS
+# Install script for MacOS using Homebrew
 # To execute: save and `chmod +x ./MacInstalls.sh` then `./MacInstalls.sh`
 
 echo "Installing Homebrew package manager"
@@ -16,16 +16,21 @@ brew install --cask discord
 brew install --cask sensiblesidebuttons
 
 # Dev tools
-brew install --cask iterm2
-brew install --cask dotnet-sdk
-brew install --cask git
-brew install --cask slack
-brew install --cask microsoft-teams
-brew install --cask zoom
-brew install --cask visual-studio-code
-brew install --cask docker
-brew install --cask postman
-brew install --cask azure-data-studio
-brew install --cask kdiff3
-brew install --cask fork
-brew install nvm
+echo "Install Dev Tools? (Y/N) " 
+read devToolDecision
+
+if [[ "$devToolDecision" == "Y" || "$devToolDecision" == "y" ]]; then
+    brew install --cask iterm2
+    brew install --cask dotnet-sdk
+    brew install --cask git
+    brew install --cask slack
+    brew install --cask microsoft-teams
+    brew install --cask zoom
+    brew install --cask visual-studio-code
+    brew install --cask docker
+    brew install --cask postman
+    brew install --cask azure-data-studio
+    brew install --cask kdiff3
+    brew install --cask fork
+    brew install nvm
+fi
